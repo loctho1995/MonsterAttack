ee216512c5a8d23dc98fe5897956408c00b4c0f9
LNGitHubAT <bbsort123@gmail.com>
2015-03-22 02:08:28 -0700

    BulletType
    BulletType

--- Classes/Define.h
+++ Classes/Define.h
@@ -1,9 +1,16 @@
 #ifndef _DEFINE_H_
 #define _DEFINE_H_
-const int BULLET_VELOCITY =  480; // Toc do cua dan
+const int BULLET_VELOCITY =  480; // Toc do cua dan PIXLE / S
 enum MonsterType // Cac loai quai chua xac dinh co bao nhieu loai
 {
 	MONSTER1,
 	MONSTER2
+};
+enum BulletType
+{
+	NORMAL,
+	FIRE,
+	LIGHTING,
+	WATER
 };
 #endif
